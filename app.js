1a287c999733405ec395bc69c2714038fd71c14f
Ankush kushwaha <ankushkushwaha2828@gmail.com>
2023-04-03 13:40:54 +0530

    some basic codes

---
+++ app.js
@@ -0,0 +1,8 @@
+// export {}
+// class App {
+//     test(){
+//         console.log("function is working" )  ; 
+//     }
+// }
+// let x  = new App()  ;  
+// x.test()  ;
---
+++ app.ts
@@ -0,0 +1,14 @@
+// export {}
+// class App {
+//     test(){
+//         console.log("function is working" )  ; 
+//     }
+// }
+
+// let x  = new App()  ;  
+// x.test()  ;
+
+
+
+
+
