7a8c96d8a5f3e4dbf9e1843907283bfb9e565b67
Ankush kushwaha <ankushkushwaha2828@gmail.com>
2023-04-03 13:40:18 +0530

    arrays concept

---
+++ Arrays.js
@@ -0,0 +1,6 @@
+var data = ['ankush', 'vishal', '10'];
+data.push('Raj');
+// data.push(2992) ; 
+// data.push(true) ;  
+console.log(data);
+console.log(data.at(0));
---
+++ Arrays.ts
@@ -0,0 +1,12 @@
+let data:string[] = ['ankush' , 'vishal'  , '10'] ;
+
+data.push('Raj') ;
+// data.push(2992) ; 
+// data.push(true) ;  
+console.log(data) ; 
+
+
+console.log(data.at(0)) ; 
+
+
+
